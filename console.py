6084b18107cc9c1a8ba0840b9106c42e4c7a8d4e
Evastrings <elijahakandetea@gmail.com>
2024-05-18 14:01:44 -0700

    command prompt 0.01

---
+++ console.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+    
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Quit command to exit the program"""
+        return True
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
